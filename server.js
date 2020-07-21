861125819a405e05addcf9fc3731606411ee403a
georgeo23 <olney.george@yahoo.co.uk>
2020-07-21 15:48:37 +0100

    change to search

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ server.use(cors());
 
 server.get(`/`, (req, res) => res.send('hello world'));
 
-// server.get(`/search`, (req, res) => res.send(JSON.stringify()))
+server.get(`/search`, (req, res) => res.send(JSON.stringify()))
 
 server.listen(port, () => console.log(`server location at http://localhost:${port}/`))
