9c3fcb2185e999298b7de3220e7b1cb501da03f1
georgeo23 <olney.george@yahoo.co.uk>
2020-07-22 12:22:37 +0100

    changed html to one page that hides and unhides on click

--- client/script.js
+++ client/script.js
@@ -3,14 +3,30 @@ luckyDipButton.addEventListener('click', getRandomLink)
 const searchButton = document.getElementById('search-button')
 searchButton.addEventListener('click', getTextBoxContent)
 let textBoxContent;
+let searchHTML;
+let homeHTML;
+hideSearch()
 
+function hideSearch() {
+  searchHTML = document.querySelector('.search')
+  searchHTML.setAttribute('style', 'visibility: hidden;')
+  console.log(searchHTML)
+  homeHTML = document.querySelector('.home')
+  console.log(homeHTML)
+}
 function getTextBoxContent(e) {
+  showSearch()
   textBoxContent = document.getElementById('search').value
   console.log(textBoxContent)
   searchPageTextBox = document.getElementById('searched-text')
   console.log(searchPageTextBox)
   }
 
+
+function showSearch() {
+  homeHTML.setAttribute('style', 'visibility: hidden;')
+  searchHTML.setAttribute('style', 'visibility: visible')
+}
 async function getRandomLink(e) {
   e.preventDefault()
 
@@ -21,4 +37,32 @@ async function getRandomLink(e) {
 
 function performLuckyDip(link) {
   window.open(link, "_self")
+}
+//////////////////////////////////////////////
+
+getSearchResults()
+function getSearchResults(e) {
+
+  fetch(`http://localhost:3000/dogsearch`)
+    .then(res => res.json())
+    .then(assignLinks)
+}
+
+async function assignLinks(link) {
+  let searchALink;
+  fetch(`http://localhost:3000/doginfo`)
+    .then(res => res.json())
+    .then(writeAContent)
+  for (i = 0; i < 10; i++) {
+    searchALink = document.getElementById(`result${i}`)
+    searchALink.setAttribute('href', `${link[i]}`)
+  }
+
+}
+
+function writeAContent(info) {
+  for (i = 0; i < 10; i++) {
+    searchALink = document.getElementById(`result${i}`)
+    searchALink.textContent = info[i]
+  }
 }
